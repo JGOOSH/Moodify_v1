0e2866cb43fec69f5b25f4916aa62ada528f2175
Marc Matvienko <m.matvienko@utexas.edu>
2017-10-28 22:21:32 -0500

    scraped

--- data_scrape.py
+++ data_scrape.py
@@ -4,18 +4,19 @@ import json
 
 # initial setup, currently using hard-coded authorization
 payload = {'limit' : 50, 'offset' : 0}
-headers = {'Accept' : 'application/json', 'Authorization' : 'Bearer BQD7asQomCVVLXYjy3LDA5wFbWf_atAdLDbersQG4BQsrVpSUJ1JS5A7a1z4qEDMcFjnTv-OLDdbEpKiTieeR36SjDAbXs8or1iTlmeLYw6RkrFKKVmbOXx_bJF3oGHDe18ouZztXsN-k4ffm16IMBULWNVz5DS8gw4bz3sKDPY'}
+headers = {'Accept' : 'application/json', 'Authorization' : 'Bearer BQBOQ9_Lpese2Qbnfaw5ENn6ZL9JoxTZISuII222wnVebcWQmdCv1PBrfph-cqRu6mliUQ3d802bA-CBw3TewHZeh_UtGCfltd8BzmgOzxPM2CorsQiW1-hDjP3eeN1cnei1kLyxdxQFsIq6VX2TPYxAgWjj'}
 track_request = requests.get('https://api.spotify.com/v1/me/tracks', params=payload, headers=headers)
 data = track_request.json()
 total = data['total']
 count = 0
 dict_data = []
-
+fi = open("nameIdRelation.txt", "w")
 # run the analysis until reaching the end of the list
 while total > 0:
     total -= 50
     for x in data['items']:
         id = (x['track']['id'])
+        name = x['track']['name']
         url = 'https://api.spotify.com/v1/audio-features/'
         url = url + id
         feature_request = requests.get(url, headers=headers)
@@ -29,9 +30,15 @@ while total > 0:
             'tempo' : feature['tempo'],
             'valence' : feature['valence']
         }
-        dict_data.append(data);
+       
+        fi.write("id: {} | name: {}\n".format( feature['id'] ,name))
+        print data
+        dict_data.append(data)
     if total > 0 :
         payload['offset'] += 50
+        
+    track_request = requests.get('https://api.spotify.com/v1/me/tracks', params=payload, headers=headers)
+    data = track_request.json()
 
 # input
 # energy, loudness, mode, speechiness, tempo, valence
