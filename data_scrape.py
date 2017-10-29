770f1cdffc7254702cea1d5d54bfad4e6006c3da
JGOOSH <jgoo@utexas.edu>
2017-10-28 19:52:14 -0500

    functionality done

--- data_scrape.py
+++ data_scrape.py
@@ -2,11 +2,41 @@ import sys
 import requests
 import json
 
-payload = {'limit' : 1}
-headers = {'Accept' : 'application/json', 'Authorization' : 'Bearer BQDKABD1wSwzeuC3krGxIU4SckLx0l2uFi_WLoBCnys1t0uNfG3wMEH2Hh-FTA_kf91hyKNYnBJxHIcTQT9mGwvcVENMssGw1E65c1zjrqmkfUPi6Nh0zzA68ZvgPri-CMQCl5n3khIcQMiYq_iRIQUxC5FLeZ46vFDYXBamIsI'}
+# initial setup, currently using hard-coded authorization
+payload = {'limit' : 50, 'offset' : 0}
+headers = {'Accept' : 'application/json', 'Authorization' : 'Bearer BQD7asQomCVVLXYjy3LDA5wFbWf_atAdLDbersQG4BQsrVpSUJ1JS5A7a1z4qEDMcFjnTv-OLDdbEpKiTieeR36SjDAbXs8or1iTlmeLYw6RkrFKKVmbOXx_bJF3oGHDe18ouZztXsN-k4ffm16IMBULWNVz5DS8gw4bz3sKDPY'}
 r = requests.get('https://api.spotify.com/v1/me/tracks', params=payload, headers=headers)
-
-# data = json.loads(r.text)
 data = r.json();
-print(data['items']['name'])
-# print(r.text);
+total = data['total']
+count = 0;
+
+#run first iteration
+while total > 0:
+    total -= 50
+    for x in data['items']:
+        id = (x['track']['id'])
+        url = 'https://api.spotify.com/v1/audio-features/'
+        url = url + id
+        feature_request = requests.get(url, headers=headers)
+        print(feature_request.text)
+        count += 1
+    if total > 0 :
+        payload['offset'] += 50
+
+print (count)
+
+#total = data['total']
+# data = json.loads(r.text)
+#print(data['items'][0]['track']['name'])
+#print(data['total'])
+#name =(data['items'][0]['track']['id'])
+
+# url = 'https://api.spotify.com/v1/audio-features/'
+# url = url + name
+# a = requests.get(url, headers = headers)
+#print(a.url);
+#print(name)
+#print(a.text)
+
+#print(len(data['items']))
+#print(r.text)
