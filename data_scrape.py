947a828fe060f1bebb837e4baf8c4874241c1b7d
JGOOSH <jgoo@utexas.edu>
2017-10-28 21:01:19 -0500

    data scraping done

--- data_scrape.py
+++ data_scrape.py
@@ -5,12 +5,13 @@ import json
 # initial setup, currently using hard-coded authorization
 payload = {'limit' : 50, 'offset' : 0}
 headers = {'Accept' : 'application/json', 'Authorization' : 'Bearer BQD7asQomCVVLXYjy3LDA5wFbWf_atAdLDbersQG4BQsrVpSUJ1JS5A7a1z4qEDMcFjnTv-OLDdbEpKiTieeR36SjDAbXs8or1iTlmeLYw6RkrFKKVmbOXx_bJF3oGHDe18ouZztXsN-k4ffm16IMBULWNVz5DS8gw4bz3sKDPY'}
-r = requests.get('https://api.spotify.com/v1/me/tracks', params=payload, headers=headers)
-data = r.json();
+track_request = requests.get('https://api.spotify.com/v1/me/tracks', params=payload, headers=headers)
+data = track_request.json()
 total = data['total']
-count = 0;
+count = 0
+dict_data = []
 
-#run first iteration
+# run the analysis until reaching the end of the list
 while total > 0:
     total -= 50
     for x in data['items']:
@@ -18,25 +19,19 @@ while total > 0:
         url = 'https://api.spotify.com/v1/audio-features/'
         url = url + id
         feature_request = requests.get(url, headers=headers)
-        print(feature_request.text)
-        count += 1
+        feature = feature_request.json()
+        data = {
+            'id' : feature['id'],
+            'energy' : feature['energy'],
+            'loudness' : feature['loudness'],
+            'mode' : feature['mode'],
+            'speechiness' : feature['speechiness'],
+            'tempo' : feature['tempo'],
+            'valence' : feature['valence']
+        }
+        dict_data.append(data);
     if total > 0 :
         payload['offset'] += 50
 
-print (count)
-
-#total = data['total']
-# data = json.loads(r.text)
-#print(data['items'][0]['track']['name'])
-#print(data['total'])
-#name =(data['items'][0]['track']['id'])
-
-# url = 'https://api.spotify.com/v1/audio-features/'
-# url = url + name
-# a = requests.get(url, headers = headers)
-#print(a.url);
-#print(name)
-#print(a.text)
-
-#print(len(data['items']))
-#print(r.text)
+# input
+# energy, loudness, mode, speechiness, tempo, valence
